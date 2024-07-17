aded550b33c91f93eb2d1bfb8944a27b6aa6ea70
Lutz Fischer <lfischer@staffmail.ed.ac.uk>
2024-07-17 15:35:09 +0200

    Some changes to detecting currently used java version and path

--- src/main/java/org/rappsilber/utils/JavaUtils.java
+++ src/main/java/org/rappsilber/utils/JavaUtils.java
@@ -5,6 +5,10 @@
 package org.rappsilber.utils;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -42,6 +46,7 @@ public class JavaUtils {
     public static int getJavaMajorVersion() {
         // get the version string from property
         String version = System.getProperty("java.version");
+        Integer  mv = null;
         
         // there was a change from 1.x.y to x.y
         if(version.startsWith("1.")) {
@@ -49,9 +54,48 @@ public class JavaUtils {
             version = version.substring(2, 3);
         } else {
             // new version string
-            int dot = version.indexOf(".");
-            if(dot != -1) { version = version.substring(0, dot); }
-        } return Integer.parseInt(version);
+            Pattern p = java.util.regex.Pattern.compile("^[0-9]+");
+            Matcher m = p.matcher(version);
+            if (m.find()) {
+                version = m.group();
+            } 
+                
+        } 
+        try {
+             mv  = Integer.parseInt(version);
+        } catch (NumberFormatException e) {
+        }
+        if (mv == null) {
+            try {
+                 mv  = Integer.parseInt(System.getProperty("java.specification.version"));
+            } catch (NumberFormatException e) {
+            }
+        }
+        if (mv == null) {
+            try {
+                 version = System.getProperty("java.runtime.version");
+                if(version.startsWith("1.")) {
+                    // old version string
+                    version = version.substring(2, 3);
+                } else {
+                    // new version string
+                    Pattern p = java.util.regex.Pattern.compile("^[0-9]+");
+                    Matcher m = p.matcher(version);
+                    if (m.find()) {
+                        version = m.group();
+                    } 
+
+                } 
+                mv =Integer.parseInt(version);
+                 
+            } catch (NumberFormatException e) {
+            }
+        }
+        if (mv == null) {
+            Logger.getLogger(JavaUtils.class.getName()).log(Level.WARNING,"Can't detect java version - assume 11");
+            return 11;
+        }
+        return mv;
         
     }
     
